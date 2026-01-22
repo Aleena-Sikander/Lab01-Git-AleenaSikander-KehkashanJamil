581c395661248cf31721129a36ef59c3ae9710e2
Aleena-Sikander <as08581@st.habib.edu.pk>
2026-01-22 11:43:16 +0500

    update task4b

--- task4b.s
+++ task4b.s
@@ -4,36 +4,35 @@ li x2 2
 li x3 3
 
 # array a:
-sw x11 0x100(x0)
-sw x12 0x101(x0)
-sw x13 0x102(x0)
-sw x14 0x103(x0)
-
-li x21 2
-li x22 4
-li x23 6
-li x24 8
+sw x1 0x100(x0)
+sw x1 0x100(x1)
+sw x1 0x100(x2)
+sw x1 0x100(x3)
 
 # array b:
-sw x21 0x200(x0)
-sw x22 0x201(x0)
-sw x23 0x202(x0)
-sw x24 0x203(x0)
-lb x30 0x100(x0)
-lb x31 0x200(x0)
-add x5, x30, x31
+sb x2 0x200(x0)
+sb x2 0x200(x1)
+sb x2 0x200(x2)
+sb x2 0x200(x3)
+
+lb x5 0x100(x0)
+lb x9 0x200(x0)
+add x13, x5, x9
+
+lb x6 0x100(x1)
+lb x10 0x200(x1)
+add x14, x6, x10
+
+lb x7 0x100(x2)
+lb x11 0x200(x2)
+add x15, x7, x11
+
+lb x8 0x100(x3)
+lb x12 0x200(x3)
+add x16, x8, x12
 
 # array c:
-sw x5 0x300(x0)
-lb x28 0x101(x0)
-lb x29 0x201(x0)
-add x2, x28, x29
-sw x2 0x301(x0)
-lb x26 0x102(x0)
-lb x27 0x202(x0)
-add x3, x26, x27
-sw x3 0x302(x0)
-lb x24 0x103(x0)
-lb x25 0x203(x0)
-add x4, x24, x25
-sw x4 0x303(x0)
+sb x13 0x300(x0)
+sb x14 0x300(x1)
+sb x15 0x300(x2)
+sb x16 0x300(x3)
